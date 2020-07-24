e7a510744cb5139c2ce07b4ce8e03c3ddb146a1a
Babken Nurijanyan <babkenn@gmail.com>
2020-07-24 11:43:35 -0400

    Updaated Modal names

--- src/components/card.js
+++ src/components/card.js
@@ -2,7 +2,7 @@ import React from "react";
 
 
 import { Card} from "react-bootstrap";
-import {HomeModal}  from "./Views/NavButton"
+import {HomeModal}  from "./Views/HomeModal"
 
 import {
   BsFillHouseFill,
